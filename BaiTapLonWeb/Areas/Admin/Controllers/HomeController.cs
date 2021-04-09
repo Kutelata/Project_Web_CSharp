07ac28fe9597c2de53b9d40f045389ef9c1b7525
HoangCaoLong <hclong2k@gmail.com>
2021-04-10 00:10:05 +0700

    Index Category (Done)

--- BaiTapLonWeb/Areas/Admin/Controllers/HomeController.cs
+++ BaiTapLonWeb/Areas/Admin/Controllers/HomeController.cs
@@ -21,5 +21,9 @@ namespace BaiTapLonWeb.Areas.Admin.Controllers
         {
             return View();
         }
+        public ActionResult Test()
+        {
+            return View();
+        }
     }
 }
