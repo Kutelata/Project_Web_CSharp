13f54d6d5cf516d4dc803d927ccb6d2d68ba0b03
Hoang Cao long <hclong2k@gmail.com>
2021-04-21 11:56:52 +0700

    Divide Success

--- WaxStore/Controllers/HomeController.cs
+++ WaxStore/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace BaiTapLonWeb.Controllers
+namespace WaxStore.Controllers
 {
     public class HomeController : Controller
     {
